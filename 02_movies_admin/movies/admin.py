855319244edfa30d3b1794cc9bdc7a2554423a6a
Damir Kambarov <damir.kambarov@qualixdev.com>
2022-04-03 08:49:54 +0500

    model created and registered in admin.py

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Filmwork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmWorkInline, PersonFilmWorkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_filter = ('type', 'title',)
+    search_fields = ('title', 'description', 'id')
--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,3 +1,93 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class FilmworkType(models.TextChoices):
+    MOVIE = 'mov', _('movie')
+    TV_SHOW = 'tvs', _('tv show')
+
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField(_('name'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+
+    class Meta:
+        db_table = "content\".\"genre"
+        verbose_name = _('genre')
+        verbose_name_plural = _('genres')
+
+    def __str__(self):
+        return self.name
+
+
+class Filmwork(UUIDMixin, TimeStampedMixin):
+    title = models.CharField(_('title'), max_length=100)
+    description = models.CharField(_('description'), max_length=255, blank=True)
+    creation_date = models.DateField(auto_now_add=True)
+    rating = models.FloatField(_('rating'), blank=True,
+                               validators=[MinValueValidator(0),
+                                           MaxValueValidator(100)])
+    type = models.CharField(_('type'), max_length=3, choices=FilmworkType.choices)
+    genres = models.ManyToManyField(Genre, through='GenreFilmwork')
+    file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
+
+    class Meta:
+        db_table = "content\".\"filmwork"
+        verbose_name = _('film work')
+        verbose_name_plural = _('film works')
+
+    def __str__(self):
+        return self.title
+
+
+class GenreFilmwork(UUIDMixin):
+    filmwork = models.ForeignKey(Filmwork, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"genre_filmwork"
+        verbose_name = _('filmwork genre')
+        verbose_name_plural = _('filmwork genres')
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.CharField(_('full name'), max_length=100)
+
+    class Meta:
+        db_table = "content\".\"person"
+        verbose_name = _('person')
+        verbose_name_plural = _('people')
+
+    def __str__(self):
+        return self.full_name
+
+
+class PersonFilmwork(UUIDMixin):
+    filmwork = models.ForeignKey(Filmwork, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role = models.TextField(_('role'), null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"person_filmwork"
+        verbose_name = _('filmwork person')
+        verbose_name_plural = _('filmwork people')
